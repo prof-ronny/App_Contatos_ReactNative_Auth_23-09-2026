6d0eb1a4e31811ec61b71ea2f8d2c5ecbf1095c0
Carlos Ronny de Sousa <135059087+prof-ronny@users.noreply.github.com>
2026-09-23 13:51:19 +0000

    Projeto Completo

---
+++ app/contatos/novo.tsx
@@ -0,0 +1,25 @@
+import { View, Alert } from 'react-native';
+import { useRouter } from 'expo-router';
+import FormContato from '../../components/FormContato';
+import api, { loadAuthToken } from '../../lib/api';
+import { global } from '../../styles/global';
+
+export default function NovoContato() {
+  const router = useRouter();
+
+  const salvar = async (dados: any) => {
+    try {
+      await loadAuthToken();
+      await api.post('/contatos', dados); // { nome, ..., fotoId }
+      router.back();
+    } catch (err: any) {
+      Alert.alert('Erro', err?.response?.data?.mensagem || 'Falha ao criar contato');
+    }
+  };
+
+  return (
+    <View style={global.container}>
+      <FormContato onSubmit={salvar} />
+    </View>
+  );
+}
